5551c61b5ad2ade14411a1fc22b0f04303cdbe75
topaz11100 <topaz11100@gmail.com>
2024-04-26 14:47:07 +0900

    테스트할것

--- spider/spider.ino
+++ spider/spider.ino
@@ -19,62 +19,85 @@ Servo leg[4];
 Servo foot[4];
 
 const int speed = 1;
-const int walk_arr[4] = {1,2,3,4};
 
-void init_motor(){
-  for(int i=0; i<4; i+=1){
-    leg[i].attach(i+4);
-    foot[i].attach(i+8);
+// _limit[위치][모드] = {{앞쪽/서는방향한계, 뒤쪽/눕는방향한계}...}
+// 측정필요
+const int leg_limit[4][2] =  { { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 } };
+const int foot_limit[4][2] = { { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 } };
+
+void init_motor() {
+  for (int i = 0; i < 4; i += 1) {
+    leg[i].attach(i + 4);
+    foot[i].attach(i + 8);
   }
 }
 
-void read_motor(){
-  for(int i=0; i<4; i+=1){
-    String temp = String(i) + "th motor angle "
+void read_motor() {
+  for (int i = 0; i < 4; i += 1) {
+    String temp = String(i) + "th motor angle " +
                   "leg : " + String(leg[i].read()) + " "
                   "foot : " + String(foot[i].read());
     Serial.println(temp);
   }
 }
 
-void lay(int speed){
-  int temp[4] = {0,0,0,0};
+void lay(int speed) {
+  int temp[4] = { foot_limit[0][1], foot_limit[1][1], foot_limit[2][1], foot_limit[3][1] };
   move_arr(foot, temp, speed);
 }
 
-void stand(int speed){
-  int temp[4] = {180,180,180,180};
+void stand(int speed) {
+  int temp[4] = { foot_limit[0][0], foot_limit[1][0], foot_limit[2][0], foot_limit[3][0] };
   move_arr(foot, temp, speed);
 }
 
-void move_arr(Servo motor[], int arr[], int speed){
-  bool check[4] = {false, false, false, false};
-  while (!(check[0]&&check[1]&&check[2]&&check[3])){
-    for(int i=0; i<4; i+=1){
+void move_arr(Servo motor[], int arr[], int speed) {
+  bool check[4] = { false, false, false, false };
+  while (!(check[0] && check[1] && check[2] && check[3])) {
+    for (int i = 0; i < 4; i += 1) {
       int temp = motor[i].read();
-      if     (temp > arr[i]) motor[i].write(temp - 1);
-      else if(temp < arr[i]) motor[i].write(temp + 1);
-      else check[i] = true;
+      if      (temp > arr[i]) motor[i].write(temp - 1);
+      else if (temp < arr[i]) motor[i].write(temp + 1);
+      else    check[i] = true;
       delay(speed);
     }
   }
 }
 
-void move_angle(Servo motor, int angle, int speed){
-  while(true){
+void move_legfoot(int num, int arr[], int speed) {
+  bool check[2] = { false, false };
+  while(!(check[0] && check[1])) {
+    int temp[2] = { leg[num].read(), foot[num].read() };
+
+    if      (temp[0] > arr[0]) leg[num].write(temp[0] - 1);
+    else if (temp[0] < arr[0]) leg[num].write(temp[0] + 1);
+    else    check[0] = true;
+
+    if      (temp[1] > arr[1]) leg[num].write(temp[1] - 1);
+    else if (temp[1] < arr[1]) leg[num].write(temp[1] + 1);
+    else    check[1] = true;
+
+    delay(speed);
+  }
+}
+
+void move_angle(Servo motor, int angle, int speed) {
+  while (true) {
     int temp = motor.read();
-    if(temp > angle) motor.write(temp - 1);
-    else if(temp < angle) motor.write(temp + 1);
+    if      (temp > angle) motor.write(temp - 1);
+    else if (temp < angle) motor.write(temp + 1);
     else return;
     delay(speed);
   }
 }
 
 void test(int speed){
-  move_angle(leg[2], 140, speed);
-  move_angle(foot[2], 0, speed);
-  move_angle(leg[2], 0, speed);
-  move_angle(foot[2], 180, speed);
+  int forward[4] = {leg_limit[0][0], leg_limit[0][1], leg_limit[0][2], leg_limit[0][3]};
+  int back[4] =    {leg_limit[0][0], leg_limit[0][1], leg_limit[0][2], leg_limit[0][3]};
+  move_arr(leg, forward, speed);
+  stand(speed);
+  move_arr(leg, back, speed);
+  lay(speed);
 }
 
 void setup() {
@@ -82,8 +105,8 @@ void setup() {
   init_motor();
 }
 
-char receive(){
-  if(Serial.available()){
+char receive() {
+  if (Serial.available()) {
     char temp = Serial.read();
     Serial.println("Received char : " + temp);
     return temp;
@@ -92,6 +115,8 @@ char receive(){
 
 void loop() {
   char received = receive();
+  
   test(speed);
-  read_motor(); 
+  
+  read_motor();
 }
