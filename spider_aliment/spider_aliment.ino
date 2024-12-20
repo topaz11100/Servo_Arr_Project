8d8966b69f92108e9aac596271e7dc02013bde0b
topaz11100 <76610871+topaz11100@users.noreply.github.com>
2024-12-20 15:29:16 +0900

    Update spider_aliment.ino

--- spider_aliment/spider_aliment.ino
+++ spider_aliment/spider_aliment.ino
@@ -6,9 +6,9 @@ const int motor_count = 8;
 Servo_vector spider{motor_count};
 const int spider_pin[motor_count] = {4,5,6,7,8,9,10,11};
 const int speed = 500;
+int angle[motor_count] = {90,90,90,90,90,90,90,90};
 
 Protocol p{',', motor_count};
-int angle[motor_count] = {90,90,90,90,90,90,90,90};
 
 void setup()
 {
@@ -17,19 +17,11 @@ void setup()
   spider.move_arr(angle, speed);
 }
 
-void fill_angle()
-{
-  for(int i = 0; i < motor_count; i++)
-  {
-    angle[i] = p[i].toInt();
-  }
-}
-
 void loop()
 {
   String input = receive_String('\n');
   p.strip(input);
-  fill_angle();
+  p.fillintarr(angle);
   spider.move_arr(angle, speed);
 }
 
